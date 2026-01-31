870cc354e6df0891c00b8eb91a5f3f2481fa8b48
Ganesh_chavan <ganeshrao416312@gamil.com>
2026-01-31 16:29:34 +0530

    02-component project done

---
+++ 02-component/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+
+import React from 'react'
+import Card from './component/Card'
+
+const App = () => {
+  return (
+    <div>
+      <Card />
+      <h1>i am from App....</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ 02-component/src/component/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <h1>hi i am from card.....</h1>
+    </div>
+  )
+}
+
+export default Card
